8a763876fd82ceb09a4fec7babcea55927f5634f
Mikolaj Mikolajewski <mikolaj.jewski@gmail.com>
2023-06-27 23:51:17 +0100

    solution

--- src/main/java/pl/javastart/validationtask/HomeController.java
+++ src/main/java/pl/javastart/validationtask/HomeController.java
@@ -1,12 +1,22 @@
 package pl.javastart.validationtask;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.net.http.HttpClient.Redirect;
 
 @Controller
+@SessionAttributes("myObject")
 public class HomeController {
+
+    private RegisterFormDto savedForm;
 
     @GetMapping("/")
     public String home(Model model) {
@@ -15,7 +25,18 @@ public class HomeController {
     }
 
     @PostMapping("/register")
-    public String register(RegisterFormDto registerFormDto, Model model) {
+    String register(@Valid @ModelAttribute("data") RegisterFormDto registerFormDto, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("data", registerFormDto);
+            return "home";
+        } else {
+            redirectAttributes.addFlashAttribute("registerFormDto", registerFormDto);
+            return "redirect:/success";
+        }
+    }
+
+    @GetMapping("/success")
+    String approved(@ModelAttribute("registerFormDto") final RegisterFormDto registerFormDto, Model model) {
         model.addAttribute("formData", registerFormDto);
         return "success";
     }
